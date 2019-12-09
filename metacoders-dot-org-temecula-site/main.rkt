df3e5749ad71de5dfc9cf3414f93e1012c4722b5
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2019-12-09 12:04:33 -0800

    Update main.rkt

--- metacoders-dot-org-temecula-site/main.rkt
+++ metacoders-dot-org-temecula-site/main.rkt
@@ -5,38 +5,11 @@
 (require metacoders-dot-org-temecula-lib
          metacoders-dot-org-lib)
 
-(define (weekend-course)
-  (course-card
-      ;#:title         "WEEKEND CLASS"
-      ;#:topic         "Point & Click Games!"
-      ;#:description   "In our Winter program, students at La Jolla will learn how to code their own whack-a-mole style games that utilize the mouse. Students will learn how to customize their game with near endless possible combinations: Are you a UFO collecting cows? Or a kid eating up their favorite foods and avoiding their least favorites? Or a ninja nabbing fruit and not the bombs? Their games will feature a point system, and students will code in-game items such that they gain or lose points when the player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!"
-      ;#:age-range     "K - 2nd"
-      ;#:image-url     "https://s3-us-west-1.amazonaws.com/ts-email-assets-and-stuff/DSC_0603_370_200.jpg"
-      ;#:meeting-dates (list "2/15/2020" "2/22/2020" "2/29/2020" "3/7/2020" "3/14/2020" "3/21/2020" "3/28/2020")
-      ;#:start-time    "11:00 am"
-      ;#:end-time      "12:30 pm"
-      ;#:location      "Gallaudet University"
-      ;#:address       "800 Florida Ave NE, Washington, DC 20002"
-      ;#:price         185
-      ;#:sku           "sku_G7REBMxlyd6Oh1"
-      ;#:key           "pk_test_BZvU77rH9zfNQvab1EpKB7GK00ZxANulPE"
-   ))
-
 (define (index)
   (page index.html
         (city-page
          #:city-name "Temecula"
          #:banner-url "https://images.pexels.com/photos/582938/pexels-photo-582938.jpeg"
-         #:locations-list (list
-                           (location-courses
-                            #:location-name "LOCATION 1"
-                            #:course-1 (weekend-course))
-                           ;(location-courses
-                           ; #:location-name "Gallaudet"
-                           ; #:course-1 (weekend-course)
-                           ; #:course-2 (weekend-course))
-                           )
-          
          )))
 
 (define (pages)
