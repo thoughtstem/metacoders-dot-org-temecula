4cd526a3b53d18e1b243ef58d3de78464a566c7f
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2020-01-09 11:44:28 -0800

    new skus!

--- metacoders-dot-org-temecula-site/main.rkt
+++ metacoders-dot-org-temecula-site/main.rkt
@@ -8,7 +8,7 @@
 (define/provide-course weekly-class-k-2nd-winter 
   (course
       #:topic         "Awesome Animals!"
-      #:sku           "sku_GOdyEEEkJ3XnB9"
+      #:sku           "sku_YJFLPIWTkS1FyRSKtkAcA"
       #:image-url     "https://s3-us-west-1.amazonaws.com/ts-email-assets-and-stuff/k-2.gif"
       #:description   "MetaCoders’ novel approach to computer science education is backed by the latest scientific research that shows learning how to code is more like learning a foreign language than learning math. Therefore, we leverage the entire field of language acquisition so that our students are ready for a future where they’ll need to speak the languages of computers fluently. We also keep the emphasis on FUN, so throughout the course students will earn badges and prizes as they expand their knowledge of computer science. During this session of Coding Club, students will be designing video games featuring animals - they'll learn how to customize the colors of & heal their animal game characters, and also avoid enemies!"
       #:grade-range   "K - 2nd"
@@ -26,7 +26,7 @@
 (define/provide-course weekly-class-3rd-6th-winter
   (course
       #:topic         "Survival Video Game Design"
-      #:sku           "sku_GOe2X552bFnLZp"
+      #:sku           "sku_QzQL6QOfSzirPwJTAnxZ6Q"
       #:image-url     "https://www.thoughtstem.com/img/stock-photos/survival-game-gif.gif"
       #:description   "In our Winter Saturday class, students will design & code their very own 2D survival games. Students will learn how to build critical features that make survival games like Minecraft and Fortnite fun! Students will use a powerful, typed language to code features like health bars, day-night cycles, and crafting/recipe mechanics. At the end of this course, students will be able to share their survival games with friends and family."
       #:grade-range   "3rd-6th"
@@ -44,7 +44,7 @@
 (define/provide-course weekly-class-k-2nd-spring
   (course
       #:topic         "Point & Click Games!"
-      #:sku           "sku_GOe3gCscfy3UPq"
+      #:sku           "sku_AnSYvUeQrO2RhLtMJUA"
       #:image-url     "https://www.thoughtstem.com/img/stock-photos/after-school/k2-clicker-cartoon-gif.gif"
       #:description   "In our Spring Saturday class, students will learn how to code their own whack-a-mole style games that utilize the mouse. Students will learn how to customize their game with near endless possible combinations: Are you a UFO collecting cows? Or a kid eating up their favorite foods and avoiding their least favorites? Or a ninja nabbing fruit and not the bombs? Their games will feature a point system, and students will code in-game items such that they gain or lose points when the player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!"
       #:grade-range   "K - 2nd"
@@ -62,7 +62,7 @@
 (define/provide-course weekly-class-3rd-6th-spring
   (course
       #:topic         "Battle Arena Video Game Design"
-      #:sku           "sku_GOe4tVRIqtLUDW"
+      #:sku           "sku_NJ01rwfgSUO741Ev4yLeRQ"
       #:image-url     "https://www.thoughtstem.com/img/stock-photos/after-school/battle-arena.gif"
       #:description   "In our Spring Saturday class, students will design & code their very own 2D battle arena video games. What's a battle arena game? Think Fortnite! Students will learn how to code their characters' weapons and armor, create enemies to battle, and program potions that will give their character special capabilities. At the end of this course, students will participate in a Game Jam where they'll work together to create their own unique games."
       #:grade-range   "3rd-6th"
