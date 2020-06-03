85a406c2eb7c1822159ea56a02e4c40f2e96f662
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2020-06-03 14:41:54 -0700

    making it look prettier

--- metacoders-dot-org-temecula-site/main.rkt
+++ metacoders-dot-org-temecula-site/main.rkt
@@ -14,7 +14,9 @@
          #:banner-url (prefix/pathify temecula-banner-path)
          #:alt-tag "City of Temecula, California on a cloudy day with view of pond"
 
-	 "Coming Soon!" )))
+	 (div class: "mb-0 pt-5 pb-5 text-center"
+	 	(h2 "Coming Soon!")
+	 	(p "We're prioritizing students', employees', and families' health during COVID-19. In the meantime, check out our Quarantine Coding Club using the link above!")))))
 
 (define (pages)
   (list
